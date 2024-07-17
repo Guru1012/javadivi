cea661719cdbaf2da9919234a8cc38afe8b1bece
Guruprasath <83056528+Guru1012@users.noreply.github.com>
2024-07-17 14:32:48 +0530

    Update division.java

--- src/com/division.java
+++ src/com/division.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class division {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("ENTER TWO NUMBER");
+        System.out.println("ENTER TWO NUMBER TO DIVIDE");
         int a = sc.nextInt();
         int b = sc.nextInt();
         System.out.println("Ans:"+( a/b)+" ");
