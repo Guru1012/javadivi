d24db905a2b9639cab384fbab0b7d09daa689bfd
Guru0010 <guruprasath.s2020@vitstudent.ac.in>
2024-07-17 13:37:25 +0530

    fc

---
+++ src/com/division.java
@@ -0,0 +1,12 @@
+package com;
+
+import java.util.Scanner;
+public class division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER TWO NUMBER");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Ans:"+( a/b)+" ");
+    }
+}
